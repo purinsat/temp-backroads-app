9945fb1afa27ac0ec908733a0a45947686f142a0
Purin S. <purin.sat@gmail.com>
2024-01-17 20:56:21 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Footer from './components/Footer'
+import About from './components/About'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,81 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'Saving Money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'Endless Hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'Amazing Comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'August 26th, 2020',
+    title: 'Temple Adventure',
+    info: 'Best Place In The World',
+    location: 'Thailand',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'August 26th, 2020',
+    title: 'Temple Adventure',
+    info: 'Best Place In The World',
+    location: 'Thailand',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'August 26th, 2020',
+    title: 'Temple Adventure',
+    info: 'Best Place In The World',
+    location: 'Thailand',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'August 26th, 2020',
+    title: 'Temple Adventure',
+    info: 'Best Place In The World',
+    location: 'Thailand',
+    duration: 6,
+    cost: 2100,
+  },
+]
